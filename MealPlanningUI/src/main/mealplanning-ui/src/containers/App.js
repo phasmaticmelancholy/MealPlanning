3828054f3f10605c26644606e8436d5cf3295f73
phasm <phasmaticmelancholy@gmail.com>
2023-09-29 17:19:28 -0400

    Updated schema and made modifications to ui

--- MealPlanningUI/src/main/mealplanning-ui/src/containers/App.js
+++ MealPlanningUI/src/main/mealplanning-ui/src/containers/App.js
@@ -3,6 +3,8 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 import Recipe from './Recipe';
 
 function Copyright() {
@@ -10,7 +12,7 @@ function Copyright() {
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        MealPlanning
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -18,10 +20,28 @@ function Copyright() {
   );
 }
 
+function SearchBar() {
+    var ingredients = ['Apple', 'Banana', 'Onion'];
+    return (<Autocomplete
+        multiple
+        options={ingredients}
+        getOptionLabel={(option) => option}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="standard"
+            label="Ingredients"
+            placeholder="Search Ingredients"
+          />
+        )}
+      />);
+}
+
 export default function App() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
+        <SearchBar />
         <Recipe />
         <Copyright />
       </Box>
--- db/tables/recipe.sql
+++ db/tables/recipe.sql
@@ -1,5 +1,6 @@
 create table if not exists recipe (
     recipe_id int generated always as identity,
+    name varchar not null,
     url varchar not null,
     preparation_time interval,
     primary key(recipe_id)
