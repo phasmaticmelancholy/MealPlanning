2ab6f47bf4ffbedb05537ff777ab11e652dd1876
unknown <phasmaticmelancholy@gmail.com>
2023-09-29 11:48:56 -0400

    Getting the bones of the UI ready

--- MealPlanningUI/src/main/mealplanning-ui/src/containers/App.js
+++ MealPlanningUI/src/main/mealplanning-ui/src/containers/App.js
@@ -3,7 +3,7 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
-import ProTip from './ProTip';
+import Recipe from './Recipe';
 
 function Copyright() {
   return (
@@ -22,10 +22,7 @@ export default function App() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Material UI Create React App example
-        </Typography>
-        <ProTip />
+        <Recipe />
         <Copyright />
       </Box>
     </Container>
